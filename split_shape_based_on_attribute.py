200864deca69620d3e6fdb77b3f4c1438d3427d9
zehpunktbarron <christopher.barron@posteo.de>
2015-06-25 08:20:50 +0200

    Cleaning up comments

--- split_shape_based_on_attribute.py
+++ split_shape_based_on_attribute.py
@@ -1,28 +1,25 @@
 # Script created to separate one shapefile in multiple ones by one specific
-# attribute
+# attribute. In this script based on the column "ID"
 
-# Example for a Inputfile called "my_shapefile" and a field called "my_attribute"
 import arcpy
 
 #Set Input Output variables
-inputFile = u"D:\SGJ3D_DXF-Export\dxf_lod1_stadtbezirksname.shp" #<-- CHANGE
-outDir = u"D:\SGJ3D_DXF-Export\splittet_export\\" #<-- CHANGE
+inputFile = u"D:\SGJ3D_DXF-Export\dxf_lod1_stadtbezirksname.shp"
+outDir = u"D:\SGJ3D_DXF-Export\splittet_export\\"
 
-# Reads My_shapefile for different values in the attribute
+# Reads shapefile for different values in the attribute
 rows = arcpy.SearchCursor(inputFile)
 row = rows.next()
 attribute_types = set([])
 
 while row:
-    attribute_types.add(row.ID) #<-- CHANGE my_attribute to the name of your attribute
+    attribute_types.add(row.ID)
     row = rows.next()
 
 # Output a Shapefile for each different attribute
 for each_attribute in attribute_types:
     outSHP = outDir + 'dxf_lod1_' + each_attribute + u".shp"
     print outSHP
-    arcpy.Select_analysis (inputFile, outSHP, "\"ID\" = '" + each_attribute + "'") #<-- CHANGE my_attribute to the name of your attribute
+    arcpy.Select_analysis (inputFile, outSHP, "\"ID\" = '" + each_attribute + "'")
 
 del rows, row, attribute_types
-
-#END
